35e0cd4e7e258346ad91e002c604aa80b651bb7b
Oswaldo Moya Pérez <65831229+OswaldoMoper@users.noreply.github.com>
2024-10-10 18:47:36 -0600

    WSL general configs

--- configuration.nix
+++ configuration.nix
@@ -1,52 +1,14 @@
 { config, pkgs, lib, modulesPath, inputs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./other.nix
-    ];
-  # Enable and config bootloader
-  boot.loader = {
-    efi = {
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot/efi";
-    };
-    # Using grub boot loader
-    grub = {
-      enable = true;
-      devices = ["nodev"];
-      efiSupport = true;
-    };
-    timeout = 20;
-  };
-  # Enable and configure networking and firewall
-  networking = {
-    hostName = "spartan"; #Define your hostname.
-    networkmanager = {
-      enable = true;
-    #   insertNameservers = [ "1.1.1.1" "8.8.8.8"];
-    };
-    # wireless.enable = true; # Enables wireless support via wpa_supplicant.
-    # Open ports in the firewall.
-    firewall.allowedTCPPorts = [ 3000 5432 587 5938 57621 ];
-    firewall.allowedUDPPorts = [ 5938 5353 ];
-  };
-  # Set your time zone.
-  time.timeZone = "America/Mexico_City";
-  # Select internationalisation properties.
-  i18n.defaultLocale = "es_MX.utf8";
+  imports = [
+    # include NixOS-WSL modules
+    # <nixos-wsl/modules>
+  ];
 
   services = {
   # Enable the X11 windowing system.
-    xserver = {
-      enable = true;
-    # Configure keymap in X11
-      xkb = {
-        layout = "us";
-        variant = "altgr-intl";
-      };
-    };
+    xserver.enable = true;
   # Enable the Plasma 6 Desktop Environment.
     displayManager = {
       sddm = {
@@ -55,39 +17,21 @@
       };
       defaultSession = "plasma";
     # Enable automatic login for the user.
-      autoLogin = {
-        enable = true;
-        user = "omoper";
-      };
+      # autoLogin = {
+      #   enable = true;
+      #   user = "omoper";
+      # };
     };
     desktopManager.plasma6.enable = true;
-  # Enable CUPS to print documents.
-    printing.enable = true;
   };
-  # Enable and config sound
-  sound = {
-    enable =  true;
-    mediaKeys.enable = true;
-  };
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-  # hardware.pulseaudio.enable = true;
-  # Configure Bluetooth
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
-  # Allow to use Xbox Headset's (USB or Xbox Wireless Dongle)
-  # hardware.xone.enable = true;
   # Define a user account.
   users.users.omoper = {
     isNormalUser = true;
     description = "Oscar Oswaldo Moya Perez";
     extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
-    hashedPassword = "$6$IqhGanTrCJ3Y8GMS$2.q7j7DfXCbEEo1zUNkQTsSL5JuPpZbM4AghPXdycMBL6Hond51SCECELA7ufpbdrlq/u5UY/91Ph4Pu5Q/GW.";
+    # Declare a password it's not recommended in WSL 
+    # hashedPassword = "your hashedPassword";
+    # password = "your password";
     shell = pkgs.zsh;
     # packages = with pkgs; [
     #   microsoft-edge
@@ -98,7 +42,11 @@
     config.allowUnfree = true;
     config.permittedInsecurePackages = [
       "haskell.compiler.ghc924"
-      "haskell.compiler.ghc924Binary"
+      "haskell.compiler.ghc966"
+    ];
+    overlays = [
+      # This is to install emacs-overlys and be able to configure doomemacs 
+      (import (builtins.fetchTarball https://github.com/nix-community/emacs-overlay/archive/master.tar.gz))
     ];
   };
   # List packages installed in system profile.
@@ -126,7 +74,6 @@
     tree
     gnumake
     gmp
-    # git
     # Requisites for my work
     any-nix-shell
     cabal-install
@@ -142,30 +89,20 @@
     insomnia
     lambda-mod-zsh-theme
     microsoft-edge
-    # msmtp
     nix-direnv-flakes
     nix-prefetch-git
     oh-my-zsh
-    # postgresql
     sox
     # sshpass
     stylish-haskell
     zlib
-    # zsh
     # Requsites for doomemacs
-    emacs
+    emacs-git
     ripgrep
     coreutils
     fd
     clang
     tmux
-  # Optional packages
-    # Requisites for xone
-    # linuxKernel.packages.linux_zen.xone
-    # cabextract
-    spotify
-    obs-studio
-    krita
   ];
 
   environment.pathsToLink = [
@@ -174,6 +111,7 @@
 
   programs = {
   # Enable and config Zsh
+    nix-index.enableZshIntegration = true;
     zsh = {
       enable = true;
       enableCompletion = true;
@@ -191,20 +129,6 @@
         any-nix-shell zsh --info-right | source /dev/stdin
       '';
       };
-  # Enable and config msmtp
-    msmtp = {
-      enable = true;
-      accounts.default = {
-        tls  = true;
-        auth = true;
-        # auth = "SCRAM-SHA-256";
-        host = "smtp.gmail.com";
-        port = 587;
-        from = "oswaldomoyap@gmail.com";
-        user = "oswaldomoyap@gmail.com";
-        passwordeval = "cat ./password.txt";
-      };
-    };
   # Enable brightness monitoring
     light.enable = true;
   # Enable and config git
@@ -215,17 +139,9 @@
           name = "MOPER";
           email = "oswaldomoyap@gmail.com";
         };
-        aliases = {
-          crf = "commit 'Refactor'";
-          spo = "stash pop";
-          spu = "stash push";
-          ssp = "stash show -p";
-          sli = "stash list";
-          };
       };
     };
-  };
-  # Enable the OpenSSH daemon.
+  };# Enable the OpenSSH daemon.
   services = {
     openssh.enable = true;
     sshd.enable = true;
@@ -233,34 +149,32 @@
   # List services that you want to enable:
   services.postgresql = {
       enable = true;
-      # package = pkgs.postgresql_12;
+      # package = pkgs.postgresql_15;
       enableTCPIP = true;
       authentication = pkgs.lib.mkOverride 10 ''
-        local all all trust
-        host all all ::1/128 trust
+        #type database DBUser address      auth-method
+        local all      all                 trust
+        # ipv4
+	      host all       all    127.0.0.1/32 trust
+        # ipv6
+        host all       all    ::1/128      trust
       '';
       initialScript = pkgs.writeText "backend-initScript" ''
         CREATE ROLE analyzer WITH LOGIN PASSWORD 'anapass';
         CREATE DATABASE aanalyzer_yesod;
         GRANT ALL PRIVILEGES ON DATABASE aanalyzer_yesod TO analyzer;
+	      GRANT ALL ON SCHEMA public TO analyzer;
       '';
   };
   # General Nix config
   nix = {
     settings = {
-      auto-optimise-store = true;
       allow-import-from-derivation = true;
       # Nix users config
       trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
       substituters = [ "https://cache.iog.io" ];
       allowed-users = [ "@wheel" "omoper" ];
       trusted-users = [ "root" "omoper" ];
-    };
-    # Nix derivations config
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
     };
     # Nix packages config
     package = pkgs.nixFlakes;
@@ -271,6 +185,5 @@
     '';
   };
   # General Nixos configurations
-  system.stateVersion = "24.05"; # Did you read the comment?
   system.configurationRevision = inputs.nixpkgs.lib.mkIf (inputs.self ? rev) inputs.self.rev;
 }
