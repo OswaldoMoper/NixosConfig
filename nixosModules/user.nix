335d3e9c47277dc4b25d492e4c0002d5d5d0eb63
Oswaldo Moper <65831229+OswaldoMoper@users.noreply.github.com>
2026-01-15 19:48:42 +0000

    Refactor user module

--- nixosModules/user.nix
+++ nixosModules/user.nix
@@ -1,107 +1,171 @@
-{ pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
-  tagName = "OswaldoMoper";
-in {
-  # Define a user account.
-  users.users.omoper = {
-    isNormalUser = true;
-    description = "Oswaldo Moper";
-    extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
-    # Declare a password it's not necessary in WSL 
-    # hashedPassword = "$6$IqhGanTrCJ3Y8GMS$2.q7j7DfXCbEEo1zUNkQTsSL5JuPpZbM4AghPXdycMBL6Hond51SCECELA7ufpbdrlq/u5UY/91Ph4Pu5Q/GW.";
-    shell = pkgs.zsh;
-  };
-# List programs that you want to enable:
-  programs = {
-  # Setup VSCode Remote
-    nix-ld = {
-      enable = true;
-      package = pkgs.nix-ld-rs;
-    };
-  # Enable and config Zsh
-    nix-index.enableZshIntegration = true;
-    zsh = {
-      enable = true;
-      enableCompletion = true;
-      autosuggestions.enable = true;
-      syntaxHighlighting.enable = true;
-      ohMyZsh.enable = true;
-      ohMyZsh.plugins = ["git" "sudo" "colorize" "extract" "history" "postgres"];
-      ohMyZsh.theme = "bira";
-      shellInit = ''
-      NAME=${tagName}
-      echo "welcome to NixOS, $NAME"
-
-      if [ ! ~/.zshrc ]; then
-        echo "creating ~/.zshrc"
-        touch ~/.zshrc
-      fi
-
-      REPO_PATH="/etc/nixos"
-      REPO_URL="$NAME/NixosConfig.git"
-      REPO_CO="spartanWSL"
-      if [ ! -d "$REPO_PATH/.git" ]; then
-          echo "Configuring the $REPO_PATH directory"
-          sudo rm -rf "$REPO_PATH"
-          sudo git clone --recursive https://github.com/"$REPO_URL" "$REPO_PATH"
-          sudo chmod -R u+w "$REPO_PATH"
-          sudo chown -R omoper:users "$REPO_PATH"
-          cd "$REPO_PATH"
-          if ! git config --global --get-all safe.directory | grep -q "$REPO_PATH"; then
-            git config --global --add safe.directory "$REPO_PATH"
-          fi
-          git checkout "$REPO_CO"
-          git remote set-url origin git@github.com:"$REPO_URL"
-      fi
-
-      if [ ! -S ~/.ssh/ssh_auth_sock ]; then
-        echo  "'ssh-agent' has not been started since the last reboot." \
-              "Starting 'ssh-agent' now."
-        eval "$(ssh-agent)"
-        ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
-      fi
-      export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
-      ssh-add ~/.ssh/"$NAME"_ed25519
-      ssh-add ~/.ssh/"$NAME"
-      ssh-add ~/.ssh/"$NAME"_rsa
-      # ssh-add ~/.ssh/id_rsa
-      if [ "$?" -ne "0" ]; then
-        echo  "No ssh keys have been added to your 'ssh-agent'" \
-              "since the last reboot. Adding default keys now."
-        ssh-add
-      fi
-
-      eval "$(direnv hook zsh)"
-    '';
-      promptInit = ''
-        any-nix-shell zsh --info-right | source /dev/stdin
-      '';
-      };
-  # Enable and config msmtp
-    msmtp = {
-      enable = true;
-      accounts.default = {
-        tls  = true;
-        auth = true;
-        # auth = "SCRAM-SHA-256";
-        host = "smtp.gmail.com";
-        port = 587;
-        from = "omoper@example.com";
-        user = "omoper@example.com";
-        passwordeval = "cat /home/omoper/password.txt";
-      };
-    };
-  # Enable and config git
-    git = {
-      enable = true;
-      config = {
-        user = {
-          name = tagName;
-          email = "omoper@example.com";
+  inherit (lib) mkIf mkOption mkEnableOption types mapAttrs optionalString concatStringsSep;
+in
+{
+  options.myUsers = mkOption {
+    type = types.attrsOf (types.submodule({ name, ...}: {
+      options = {
+        enable = mkEnableOption "Enable this user";
+        fullName = mkOption {
+          type = types.str;
+          default = name;
+          description = "Full name of the user";
+        };
+        password = mkOption {
+          type = types.nullOr types.str;
+          default = null;
+          description = "Plaintext password for this user.";
+        };
+        hashedPassword = mkOption {
+          type = types.nullOr types.str;
+          default = null;
+          description = "Hashed password for this user.";
+        };
+        email = mkOption {
+          type = types.str;
+          default = "";
+          description = "General email for this user (used as default for Git and msmtp)";
+        };
+        home = {
+          enable = mkEnableOption "Enable Home Manager for this user";
+          profiles = mkOption {
+            type = types.listOf types.str;
+            default = [];
+            description = "Home Manager profiles to import";
+          };
+          git = {
+            enable = mkEnableOption "Enable Git configuration";
+            email = mkOption {
+              type = types.str;
+              default = config.myUsers.${name}.email;
+              description = "Email for Git commits";
+            };
+            tag = mkOption{
+              type = types.str;
+              default = "";
+              description = "GitHub username";
+            };
+          };
+          msmtp = {
+            enable = mkEnableOption "Enable msmtp";
+            email = mkOption {
+              type = types.str;
+              default =  config.myUsers.${name}.email;
+              description = "Email for msmtp outgoing mail";
+            };
+            passwordFile = mkOption {
+              type = types.str;
+              default = "";
+              description = "Path to password file";
+            };
+          };
+          sshKeys = {
+            enable = mkEnableOption "Enable SSH key auto-loading";
+            baseName = {
+              enable = mkEnableOption "Enable basename keys";
+              name = mkOption {
+                type = types.str;
+                default = "";
+                description = "Base name for SSH keys";
+              };
+            };
+            names = mkOption {
+              type = types.listOf types.str;
+              default = [];
+              description = "SSH key names for auto-loading";
+            };
+          };
+          vscode.enable = mkEnableOption "Enable VScode Remote integration";
         };
       };
-    };
+    }));
+    default = {};
   };
+  config = {
+    users.users = mapAttrs (name: cfg: mkIf cfg.enable {
+      isNormalUser = true;
+      description = cfg.fullName;
+      shell = pkgs.zsh;
+      extraGroups = [ "wheel" "networkmanager" "video" "audio" ];
+      password = mkIf (cfg.password != null) cfg.password;
+      hashedPassword = mkIf (cfg.hashedPassword != null) cfg.hashedPassword;
+    })config.myUsers;
+    programs = {
+      nix-index.enableZshIntegration = true;
+      zsh = {
+        enable = true;
+        enableCompletion = true;
+        autosuggestions.enable = true;
+        syntaxHighlighting.enable = true;
+        ohMyZsh.enable = true;
+        ohMyZsh.plugins = [ "git" "sudo" "colorize" "extract" "history" "postgres" ];
+        ohMyZsh.theme = "bira";
+        shellInit = ''
+          NAME=$USER
+          echo "welcome to NixOS, $NAME"
 
+          if [ ! ~/.zshrc ]; then
+            echo "creating ~/.zshrc"
+            touch ~/.zshrc
+          fi
+
+          eval "$(direnv hook zsh)"
+        '';
+        promptInit = ''
+          any-nix-shell zsh --info-right | source /dev/stdin
+        '';
+      };
+      nix-ld.enable = true;
+    };
+    home-manager.users = mapAttrs (name: cfg: mkIf cfg.home.enable {
+      home.stateVersion = "25.05";
+      services.ssh-agent.enable = cfg.home.sshKeys.enable;
+      programs.git = mkIf cfg.home.git.enable {
+        enable = true;
+        userName = cfg.home.git.tag;
+        userEmail = cfg.home.git.email;
+      };
+      home.file.".config/msmtp/config" = mkIf cfg.home.msmtp.enable {
+        text = ''
+          defaults
+          auth           on
+          tls            on
+          tls_trust_file /etc/ssl/certs/ca-certificates.crt
+
+          account default
+          host smtp.gmail.com
+          port 587
+          from ${cfg.home.msmtp.email}
+          user ${cfg.home.msmtp.email}
+          passwordeval "cat ${cfg.home.msmtp.passwordFile}"
+        '';
+      };
+      home.packages = mkIf cfg.home.msmtp.enable [
+        pkgs.msmtp
+      ];
+      programs.ssh = {
+        enable = true;
+        extraConfig = ''
+          AddKeysToAgent yes
+            ${optionalString cfg.home.sshKeys.enable ''
+            IdentityFile ~/.ssh/${cfg.home.sshKeys.baseName.name}_ed25519
+            IdentityFile ~/.ssh/${cfg.home.sshKeys.baseName.name}
+            IdentityFile ~/.ssh/${cfg.home.sshKeys.baseName.name}_rsa
+          ''}
+          ${concatStringsSep "\n" (map (key: "IdentityFile ~/.ssh/${key}") cfg.home.sshKeys.names)}
+        '';
+      };
+      programs.vscode.enable = cfg.home.vscode.enable;
+      imports =
+        let
+          base = ./hmProfiles;
+          existing = builtins.filter
+            (p: builtins.pathExists (base + "/${p}.nix"))
+            cfg.home.profiles;
+        in
+        map (p: base + "/${p}.nix") existing;
+    }) config.myUsers;
+  };
 }
