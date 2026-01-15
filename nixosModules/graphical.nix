c23cf6be18fb453043e887e1a5dd262311147f7d
Oswaldo Moper <65831229+OswaldoMoper@users.noreply.github.com>
2026-01-15 19:45:17 +0000

    Refactor Graphical module

--- nixosModules/graphical.nix
+++ nixosModules/graphical.nix
@@ -1,29 +1,60 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+  inherit (lib) mkIf mkOption mkEnableOption types;
+  cfg = config.graphical;
+  isWSL = cfg.mode == "WSL";
+  isGraphical = cfg.mode == "Linux";
+in
 {
-  services = {
-  # Enable the X11 windowing system.
-  #   xserver = {
-  #     enable = true;
-  #   # Configure keymap in X11
-  #     xkb = {
-  #       layout = "us";
-  #       variant = "altgr-intl";
-  #     };
-  #   };
-  # # Enable the Desktop Environment.
-  #   displayManager = {
-  #     sddm = {
-  #       enable = true;
-  #       wayland.enable = true;
-  #     };
-  #     defaultSession = "plasma";
-  #   # Enable automatic login for the user.
-  #     autoLogin = {
-  #       enable = true;
-  #       user = "omoper";
-  #     };
-  #   };
-  #   desktopManager.plasma6.enable = true;
+  options.graphical = {
+    enable = mkEnableOption "Graphical environment";
+    mode = mkOption {
+      type = types.enum [ "WSL" "Linux" ];
+      default = "Linux";
+      description = "Graphical mode: WSL or Linux";
+    };
+    keymap = mkOption {
+      type = types.str;
+      default = "us";
+      description = "Keyboard layout";
+    };
+    variant = mkOption {
+      type = types.str;
+      default = "altgr-intl";
+      description = "Keyboard layout variant";
+    };
+    fonts = mkOption {
+      type = types.listOf types.str;
+      default = [];
+      description = "Fonts to install";
+    };
+  };
+  config = mkIf cfg.enable {
+    services = {
+      xserver = {
+        enable = isGraphical;
+        xkb = {
+          layout = cfg.keymap;
+          variant = cfg.variant;
+        };
+      };
+      displayManager = {
+        sddm = {
+          enable = isGraphical;
+          wayland.enable = isGraphical;
+        };
+        defaultSession = "plasma";
+      };
+      desktopManager.plasma6.enable = isGraphical;
+    };
+    programs.light.enable = isGraphical;
+    fonts.packages = mkIf isGraphical (map (f: pkgs.${f}) cfg.fonts);
+    services.pipewire = {
+      enable = isGraphical;
+      alsa.enable = isGraphical;
+      pulse.enable = isGraphical;
+      jack.enable = isGraphical;
+    };
   };
 }
